120d1db122795c455673de1eb522b0c6ebbc0e95
caixinquan <caixinquan@dfl.com.cn>
2019-11-04 17:42:18 +0800

    feat: 为页面默认添加属性$router $service

--- page/index.js
+++ page/index.js
@@ -2,9 +2,14 @@
   * @Author: O_c
   * @Date:   2019-11-01 15:13:43
   * @Last Modified by:   O_c
-  * @Last Modified time: 2019-11-01 16:07:43
+  * @Last Modified time: 2019-11-04 17:39:41
   */
 function page(ops = {}, ...args) {
+  const app = getApp()
+
+  ops.$router = app.$router
+  ops.$service = app.$service
+
   const originOnLoad = ops.onLoad || function() {}
 
   ops.onLoad = function(options) {
